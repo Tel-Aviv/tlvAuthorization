bc0d3d5173d09b7706d5c06bcf80b14febcee0f3
Oleg Kleiman <oleg_kleyman@hotmail.com>
2019-08-29 19:07:11 +0300

    Update TLVAuthorizeAttribute.cs

--- TLVAuthorizeAttribute.cs
+++ TLVAuthorizeAttribute.cs
@@ -9,7 +9,7 @@ using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
 
-namespace Products.OAuthProvider
+namespace TLV.OAuth
 {
     public class TLVAuthorizeAttribute: AuthorizeAttribute
     {
